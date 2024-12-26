fa9cf279a3de10ea2fd6446c11f48db48f033fad
Prashant Praveen <439110@conneqtcorp.com>
2024-12-26 21:49:09 +0530

    changes for carousel

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -51,8 +51,7 @@ export default function decorate(block) {
   ul.querySelectorAll('picture > img').
   forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
-  debugger;
-  
+
   block.append(ul);
 }
 
